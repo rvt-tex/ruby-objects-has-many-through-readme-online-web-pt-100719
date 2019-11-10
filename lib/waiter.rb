0f954ffda4e1a3ea5bf8a7fc7f02ad0671818712
Ruel Tingling <rvtingling@gmail.com>
2019-11-10 08:15:14 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,8 +1,18 @@
 class Waiter
 
-
-
-
+ attr_accessor :name, :yrs_experience
+ 
+  @@all = []
+ 
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
 
 def meals
   Meal.all.select do |meal|
