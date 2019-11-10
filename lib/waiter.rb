55661cff691c1503e0aa7b62d2bb9a7eab737967
Ruel Tingling <rvtingling@gmail.com>
2019-11-10 08:18:10 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -14,8 +14,8 @@ class Waiter
     @@all
   end
 
-def new_meal(customer, total, tip=0)
-    Meal.new(waiter, self, total, tip)
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
   end
   
 def meals
